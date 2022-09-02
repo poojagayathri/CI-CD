3b0746345361c108354f7b76d86c3833080ccddf
pooja gayathri <gayathripooja56@gmail.com>
2022-09-03 01:23:37 +0530

    code correction

--- src/main/java/com/sport/SportyShoesApplication.java
+++ src/main/java/com/sport/SportyShoesApplication.java
@@ -2,14 +2,21 @@ package com.sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.sport")
-public class SportyShoesApplication {
+public class SportyShoesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportyShoesApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SportyShoesApplication.class);
 	}
 
 }
