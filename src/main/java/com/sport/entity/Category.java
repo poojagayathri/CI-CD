77b798629178f4c5381fc51f31eaa750d86b6426
pooja gayathri <gayathripooja56@gmail.com>
2022-09-03 00:04:37 +0530

    code correction

--- src/main/java/com/sport/entity/Category.java
+++ src/main/java/com/sport/entity/Category.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class Category {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String categoryName;
 
